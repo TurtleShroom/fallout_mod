c9970c0b6ebc96d0f2b17978ea3d88e2d5e348a9
Ben Rouncefield <ben.rouncefield@gmail.com>
2018-04-29 10:55:04 +0100

    1.25 Progress
    - Tidied up structure
    - Changed buildings

--- common/defines/00_fallout.lua
+++ common/defines/00_fallout.lua
@@ -3,70 +3,109 @@
 --------------------------------
 NDefines.NGame.START_DATE = "2280.1.1"
 NDefines.NGame.END_DATE = "9999.1.1"
-
-NDefines.NEngine.EVENT_PROCESS_OFFSET = 20
-NDefines.NCountry.MAX_CROWN_COLONIES = 9999
-
---------------------------------
--- General
---------------------------------
-NDefines.NCountry.MIGRATION_COOLDOWN = 240
-NDefines.NDiplomacy.ALLOW_ESTATE_AND_PARLIAMENT_SEAT_IN_PROVINCE = 1
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "9999.1.1"
+NDefines.NGame.GREAT_POWER_FRENZY_START = "9999.1.1"
 
 --------------------------------
--- Ruler
+-- Compatibility / Disabled 
 --------------------------------
-NDefines.NCountry.MAX_EXTRA_PERSONALITIES = 3
+NDefines.NNationDesigner.MANDATE_SUBCONTINENT_NAME = "west_coast_superregion"
+NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 0
+NDefines.NCountry.FEMALE_ADVISOR_CHANCE = 0
+NDefines.NCountry.MONARCH_DEATH = 0
+NDefines.NCountry.ALLOW_ZERO_BASE_VALUES = 1
+
+--------------------------------
+-- Diplomacy
+--------------------------------
+NDefines.NDiplomacy.STUDY_TECHNOLOGY_MIN_TECHS_AHEAD = 0 -- Allow power gain from Study Technology always
+
+--------------------------------
+-- Country
+--------------------------------
+NDefines.NCountry.POWER_MAX = 1000 -- Clean
+NDefines.NCountry.ALLOW_ESTATE_AND_PARLIAMENT_SEAT_IN_PROVINCE = 1
+NDefines.NCountry.MAX_EXTRA_PERSONALITIES = 3 -- Allow 3 modifiers
 NDefines.NCountry.YEARS_PER_EXTRA_PERSONALITY = 10 
-
---------------------------------
--- Technology
---------------------------------
-NDefines.NCountry.POWER_MAX = 1000 
-NDefines.NCountry.FREE_IDEA_GROUP_COST = 1.5
-NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 1
-NDefines.NCountry.TECH_TIME_COST = 0.3		                -- tech grow with 20% cost over time.
-NDefines.NCountry.TECH_AHEAD_OF_TIME = 0.1		            -- per year ahead.
-
---------------------------------
--- Power Cost
---------------------------------
+NDefines.NCountry.MAX_CROWN_COLONIES = 9999 -- Disable colonial nations
+NDefines.NCountry.MIGRATION_COOLDOWN = 240 -- Slow down migration
+NDefines.NCountry.INNOVATIVENESS_FIRST_REACHED_TECH_LEVEL = 5.0 -- Make innovativeness easier to gain
+NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 5.0 -- Make innovativeness easier to gain
+NDefines.NCountry.INNOVATIVENESS_BEHIND_IN_TECH_PENALTY = -0.05 -- Make it easier to lose innovativeness
+NDefines.NCountry.INNOVATIVENESS_ENABLE_ANY_BEHIND_IN_TECH_PENALTY = 1 -- Make it easier to lose innovativeness
+NDefines.NCountry.INNOVATIVENESS_ENABLE_PER_TECH_BEHIND_IN_TECH_PENALTY = 1 -- Make it easier to lose innovativeness
+NDefines.NCountry.INNOVATIVENESS_AHEAD_OF_TIME_BONUS = 0.1 -- Make innovativeness easier to gain
+NDefines.NCountry.FREE_IDEA_GROUP_COST = 1.5  -- Adapt NI unlocks to ideagroup system
+NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 1 -- Remove ideagroup pick restriction
+NDefines.NCountry.TECH_TIME_COST = 0.3	    -- tech grow with 20% cost over time.
+NDefines.NCountry.TECH_AHEAD_OF_TIME = 0.1  -- per year ahead.
 NDefines.NCountry.PS_ESTABLISH_SIBERIAN_FRONTIER = 50
-NDefines.NCountry.PS_FACTION_BOOST = 30 -- 10
+NDefines.NCountry.PS_FACTION_BOOST = 25
+NDefines.NCountry.EXPLOIT_ADM_INCOME = 120      -- Make exploit development more useful
+NDefines.NCountry.EXPLOIT_DIP_SAILORS = 12      -- Make exploit development more useful
+NDefines.NCountry.EXPLOIT_MIL_MANPOWER = 12     -- Make exploit development more useful
+NDefines.NCountry.EXPLOIT_COOLDOWN_MONTHS = 12  -- Make exploit development more useful
+NDefines.NCountry.ABDICATE_LEGITIMACY_THRESHOLD = 100 -- Allow abdication anytime
+NDefines.NCountry.ABDICATE_AGE_THRESHOLD = 18 -- Allow abdication for any adult ruler
+NDefines.NCountry.ABDICATE_RULING_LENGTH_THRESHOLD = 0 -- Allow abdication anytime
+NDefines.NCountry.ABDICATE_LEGITIMACY_HIT = -90 -- Make abdication much more costly
+NDefines.NCountry.ABDICATE_PRESTIGE_HIT = -90 -- Make abdication much more costly
+NDefines.NCountry.EMBRACE_INSTITUTION_COST = 5 -- Make embracement of institutions twice as expensive
+NDefines.NCountry.STATE_MAINTENANCE_DEV_FACTOR = 0.01 -- Make states more expensive to maintain
+NDefines.NCountry.STATE_MAINTENANCE_CULTURE_FACTOR = 0.5 -- Make states more expensive to maintain
+NDefines.NCountry.PARLIAMENT_BACKING_PERCENTAGE = 25
+NDefines.NCountry.PARLIAMENT_PRESTIGE_HIT = 0
+NDefines.NCountry.CULTURAL_UNION_MIN_DEV = 9999999 -- Disable cultural union (doesn't make sense with current cultures)
+NDefines.NCountry.CULTURAL_UNION_MIN_RANK = -1 -- Force dev check for cultural union
+NDefines.NCountry.ADVISOR_COST_INCREASE_PER_YEAR = 0 -- Disable cost increase for advisors
+NDefines.NCountry.PS_REDUCE_INFLATION = 100 -- Make inflation more expensive to reduce
+NDefines.NCountry.CORE_LOSE = -1 -- Cores are never lost
+NDefines.NCountry.CORE_LOSE_CULTURE_GROUP = -1 -- Cores are never lost
 
 --------------------------------
--- Government
+-- Economy
 --------------------------------
-NDefines.NCountry.SUBJECT_MAX_RANK = 2
-NDefines.NCountry.PRESTIGE_GAIN_FOR_GOV_RANK_2 = 25
-NDefines.NCountry.PRESTIGE_GAIN_FOR_GOV_RANK_3 = 25
-NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_2 = 25
-NDefines.NCountry.MIN_PRESTIGE_FOR_GOV_RANK_3 = 25
-NDefines.NCountry.MIN_DEVELOPMENT_FOR_GOV_RANK_2 = 300
-NDefines.NCountry.MIN_DEVELOPMENT_FOR_GOV_RANK_3 = 1000
-NDefines.NCountry.MAX_GOV_RANK = 3		
-
-NDefines.NDiplomacy.PARLIAMENT_BACKING_PERCENTAGE = 25
-NDefines.NDiplomacy.PARLIAMENT_PRESTIGE_HIT = 0
+NDefines.NEconomy.COLONIAL_MAINTENANCE_FACTOR = 2.0 -- Make colonisation cheaper
+NDefines.NEconomy.BASE_INTERESTS = 10.0 -- Loans more risky in the wasteland
+NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.05 -- 50% increase after all (20) mil techs taken
+NDefines.NEconomy.DECREASE_AUTONOMY_STEP = -10  -- Make autonomy changes more gradual
+NDefines.NEconomy.DECREASE_AUTONOMY_MIN = 10 
+NDefines.NEconomy.INCREASE_AUTONOMY_STEP = 10 -- Make autonomy changes more gradual
+NDefines.NEconomy.INCREASE_AUTONOMY_MAX = 90
+NDefines.NEconomy.AUTONOMY_CHANGE_DURATION = 3650 -- Allow autonomy buttons every 10 years
+NDefines.NEconomy.COLONY_MIN_AUTONOMY = 75 
+NDefines.NEconomy.OVERSEAS_MIN_AUTONOMY = 50
+NDefines.NEconomy.AUTONOMY_AT_CONQUEST_CLAIM = 25 -- Make claims better
+NDefines.NEconomy.AUTONOMY_AT_CONQUEST = 75 -- Make unclaimed conquest worse
+NDefines.NEconomy.AUTONOMY_AT_DIPLO_ANNEX = 25 -- Make diplo annex better
+NDefines.NEconomy.DEBASE_ADDED_CORRUPTION = 1 -- Make debase better
+NDefines.NEconomy.DEBASE_MAX_STORED_MONTHS = 1200 -- Why not?
 
 --------------------------------
 -- AI
 --------------------------------
 NDefines.NAI.AI_USES_HISTORICAL_IDEA_GROUPS = 0
 NDefines.NAI.AI_CONVERT_CULTURES = 1   
- 
+NDefines.NAI.AGGRESSIVENESS = 500 -- Make AI more war-happy
+NDefines.NAI.AGGRESSIVENESS_BONUS_EASY_WAR = 500 -- Make AI more war-happy
+NDefines.NAI.NAVY_BUDGET_FRACTION = 0.2 -- Reduce AI spending on navy
+NDefines.NAI.DEVELOPMENT_CAP_BASE = 30 -- Allow AI to develop more
+NDefines.NAI.PEACE_TIME_MONTHS = 24 -- Reduce AI stubbornness
+NDefines.NAI.PEACE_INCONCLUSIVE_THRESHOLD = 0 -- Allow for peace demands at 0
 --------------------------------
--- Combat and War           
+-- Military 
 --------------------------------
 NDefines.NMilitary.COSSACKS_SHOCK_DAMAGE_BONUS = 0.1
 NDefines.NMilitary.STRELTSY_FIRE_DAMAGE_BONUS = 0.1
-    
---------------------------------
--- BANNERS
---------------------------------
-NDefines.NMilitary.BANNER_DISCIPLINE_BONUS = 0.1
-NDefines.NMilitary.BANNER_AGE_MULTIPLIER = 0
-NDefines.NCountry.CORRUPTION_FROM_BANNERS = 5
+NDefines.NMilitary.FORTRESS_COST = 0.1 -- Make forts cheaper to maintain.
+NDefines.NMilitary.INFANTRY_SPEED = 1.0 -- Revert speed nerf Paradox introducted.
+NDefines.NMilitary.CAVALRY_SPEED = 1.0 -- Revert speed nerf Paradox introducted.
+NDefines.NMilitary.ARTILLERY_SPEED = 1.0 -- Revert speed nerf Paradox introducted.
+NDefines.NMilitary.FORT_PER_DEV_RATIO = 100 -- Make army trad. from forts easier to get.
+NDefines.NMilitary.MIN_MONTHLY_MANPOWER = 0.05 -- Represents difficulty in supply across wasteland
+NDefines.NMilitary.MONTHLY_REINFORCE = 0.05 -- Represents difficulty in supply across wasteland
+NDefines.NMilitary.WARSCORE_MAX_FROM_BATTLES = 80 -- Battles more important than capture in the wasteland
+NDefines.NMilitary.NAVAL_DOCTRINE_MIN_FORCE_LIMIT = 9999 -- Disable Naval Doctrines
 
 --------------------------------
 -- GOVERNMENT
@@ -193,11 +232,3 @@ NDefines.NReligion.INITIAL_ISOLATIONISM = 1	-- Level of isolationism for a newly
 --------------------------------
 NDefines.NGraphics.SHOW_MONTHLY_TAX_INCOME = 1
 
---------------------------------
--- Compatibility / Disabled 
---------------------------------
-NDefines.NNationDesigner.MANDATE_SUBCONTINENT_NAME = "west_coast_superregion"
-NDefines.NDiplomacy.NUM_OF_GREAT_POWERS = 0
-NDefines.NCountry.FEMALE_ADVISOR_CHANCE = 0
-NDefines.NCountry.MONARCH_DEATH = 0
-NDefines.NCountry.ALLOW_ZERO_BASE_VALUES = 1
